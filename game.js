d81d55bfe416e260486a67a667c6a3ddf4d66e67
Aj <ajdefano+1@gmail.com>
2023-04-17 09:14:32 -0400

    You can now control a pufferfish, and moved graphics to draw(), with some scaling

--- game.js
+++ game.js
@@ -1,32 +1,65 @@
-const canvas = document.getElementById("game-canvas");
-const ctx = canvas.getContext("2d");
+console.log('JS file loaded successfully');
 
-// load mountain images
-const mountainLayer1 = new Image();
-mountainLayer1.src = "./mountain_bg.png";
-const mountainLayer2 = new Image();
-mountainLayer2.src = "./mountain_bg2.png";
+document.addEventListener("DOMContentLoaded", function() {
+  const canvas = document.getElementById("game-canvas");
+  const container = document.getElementById("game-container");
+  canvas.width = container.clientWidth+1;
+  canvas.height = container.clientHeight;
+  const ctx = canvas.getContext("2d");
 
-// load background image
-const backgroundImage = new Image();
-backgroundImage.src = "./background.png";
+  // load images
+  const mountainLayer1 = new Image();
+  mountainLayer1.src = "./mountain_bg.png";
 
-// load foreground image
-const foregroundImage = new Image();
-foregroundImage.src = "./foreground.png";
+  const mountainLayer2 = new Image();
+  mountainLayer2.src = "./mountain_bg2.png";
 
-// draw images
-mountainLayer1.onload = function () {
-  ctx.drawImage(mountainLayer1, 0, 0);
-  mountainLayer2.onload = function () {
-    ctx.drawImage(mountainLayer2, 0, 0);
-    backgroundImage.onload = function () {
-      canvas.width = backgroundImage.width;
-      canvas.height = backgroundImage.height;
-      ctx.drawImage(backgroundImage, 0, 0);
-      foregroundImage.onload = function () {
-        ctx.drawImage(foregroundImage, 0, 0);
-      }
+  const backgroundImage = new Image();
+  backgroundImage.src = "./background.png";
+
+  const foregroundImage = new Image();
+  foregroundImage.src = "./foreground.png";
+
+  const mainCharacter = new Image();
+  mainCharacter.src = "./puffer.png";
+
+  // set initial position of the main character
+  let mainCharacterX = 50;
+  let mainCharacterY = 250;
+
+  // define function to draw the main character on the canvas
+  const draw = () => {
+    // clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    // draw the mountain images
+    ctx.drawImage(mountainLayer1, 0, 150, mountainLayer1.naturalWidth * 0.75, mountainLayer1.naturalHeight );
+    ctx.drawImage(mountainLayer2, 0, 150, mountainLayer2.naturalWidth * 0.75, mountainLayer2.naturalHeight );
+    // draw the main character
+    ctx.drawImage(foregroundImage, 0, canvas.height - foregroundImage.naturalHeight * 1.41, canvas.width, foregroundImage.naturalHeight);
+
+    ctx.drawImage(mainCharacter, mainCharacterX, mainCharacterY);
+  };
+
+  // call the draw function to draw the initial state of the game
+  draw();
+
+  // define function to update the position of the main character based on arrow key input
+  const updatePosition = (event) => {
+    if (event.keyCode === 37) { // left arrow
+      mainCharacterX -= 5;
+    } else if (event.keyCode === 39) { // right arrow
+      mainCharacterX += 5;
+    } else if (event.keyCode === 38) { // up arrow
+      mainCharacterY -= 5;
+    } else if (event.keyCode === 40) { // down arrow
+      mainCharacterY += 5;
     }
-  }
-}
+  };
+
+  // add event listener to update position of main character based on arrow key input
+  document.addEventListener("keydown", updatePosition);
+
+  // call the draw function every 10 milliseconds to update the canvas
+  setInterval(draw, 10);
+});
